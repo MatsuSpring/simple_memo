24080e61d9a627bc18cc683b4031104d99528ed2
harum <haru.mattsu0415@gmail.com>
2024-08-13 18:49:16 +0900

    メモを関数で生成するのをやめた

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import flet as ft
 
 def main(page: ft.Page):
-    # メモ部を生成する関数
-    def create_new_memo():
-        return ft.Container(
-            content=ft.Column(
-                controls=[
-                    ft.TextField(
-                        hint_text="タイトル",
-                        hint_style=ft.TextStyle(color=ft.colors.BLACK26),
-                        text_size=20,
-                        border=ft.InputBorder.UNDERLINE
-                    ),
-                    ft.TextField(
-                        hint_text="ここにメモを入力",
-                        hint_style=ft.TextStyle(color=ft.colors.BLACK26),
-                        text_size=17,
-                        border=ft.InputBorder.NONE,
-                        multiline=True      # 複数行入力可能にする
-                    ),
-                ]
-            ),
-            padding=ft.padding.symmetric(vertical=5, horizontal=15),
-            border=ft.border.all(width=2, color=ft.colors.BLACK54),
-            border_radius=20
-        )
 
     # メモを追加する関数
     def add_memo(e):
-        memo_column.controls.append(create_new_memo())
+        memo_column.controls.append(
+            # このContainerが一つのメモになっている
+            ft.Container(
+                content=ft.Column(
+                    controls=[
+                        ft.TextField(
+                            hint_text="タイトル",
+                            hint_style=ft.TextStyle(color=ft.colors.BLACK26),
+                            text_size=20,
+                            border=ft.InputBorder.UNDERLINE
+                        ),
+                        ft.TextField(
+                            hint_text="ここにメモを入力",
+                            hint_style=ft.TextStyle(color=ft.colors.BLACK26),
+                            text_size=17,
+                            border=ft.InputBorder.NONE,
+                            multiline=True      # 複数行入力可能にする
+                        ),
+                    ]
+                ),
+                padding=ft.padding.symmetric(vertical=5, horizontal=15),
+                border=ft.border.all(width=2, color=ft.colors.BLACK54),
+                border_radius=20
+            )
+        )
         memo_column.update()
 
     # メモを表示するためのColumn
